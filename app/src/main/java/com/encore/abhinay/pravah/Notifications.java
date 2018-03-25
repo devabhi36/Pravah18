2e5dc4cee3a854d73e4aaca5f7975adae7c444cb
Terra27 <hl_harshaldev@yahoo.co.in>
2018-03-25 12:35:13 +0530

    Added notification.

--- app/src/main/java/com/encore/abhinay/pravah/Notifications.java
+++ app/src/main/java/com/encore/abhinay/pravah/Notifications.java
@@ -1,11 +1,23 @@
 package com.encore.abhinay.pravah;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class Notifications extends AppCompatActivity {
     TextView title, message;
@@ -16,5 +28,50 @@ public class Notifications extends AppCompatActivity {
         setContentView(R.layout.activity_notifications);
         title = (TextView) findViewById(R.id.ntitle);
         message = (TextView) findViewById(R.id.nmessage);
+
+        new notificationCaller().execute();
+    }
+
+    private class notificationCaller extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... v) {
+
+            try {
+
+                String link = MainActivity.host + "notify.php";
+
+                URL url = new URL(link);
+                URLConnection conn = url.openConnection();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                // Read Server Response
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+
+                return sb.toString();
+            } catch (Exception e) {
+
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            title.setText(result.substring(0, result.indexOf("+")));
+            message.setText(result.substring(result.indexOf("+")+1, result.length()));
+        }
     }
 }
