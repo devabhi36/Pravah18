f892903477dd29639999d59317de91d21529b626
devabhi36 <abhinayshrivastav2@gmail.com>
2018-03-25 13:10:02 +0530

    changed day

--- app/src/main/java/com/encore/abhinay/pravah/Day3.java
+++ app/src/main/java/com/encore/abhinay/pravah/Day3.java
@@ -15,7 +15,7 @@ public class Day3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.day1, container, false);
+        return inflater.inflate(R.layout.day3, container, false);
     }
 
     @Override
