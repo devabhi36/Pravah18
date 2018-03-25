696258158f22b0cb0077cadabfc05797adbe5f60
devabhi36 <abhinayshrivastav2@gmail.com>
2018-03-25 12:13:57 +0530

    added schedule

--- app/src/main/java/com/encore/abhinay/pravah/Day3.java
+++ app/src/main/java/com/encore/abhinay/pravah/Day3.java
@@ -11,16 +11,16 @@ import android.view.ViewGroup;
  * Created by Abhinay on 06-02-2018.
  */
 
-public class Day2 extends Fragment {
+public class Day3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.day2, container, false);
+        return inflater.inflate(R.layout.day1, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("DAY 1");
+        getActivity().setTitle("DAY 2");
     }
 }
